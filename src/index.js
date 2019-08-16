e745fd6f8a1f0db3c886018f0c3ad4a1adf7f919
Tommy O' Peters <tommyopeters>
2019-08-16 04:34:59 +0100

    Sass Media Queries

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//import scss
+import "./sass/main.scss";
+
 //import Components
 import Layout from "./components/Layout/Layout";
 // import Test from "./test.js";
